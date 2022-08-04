3fc87a629f7e2c268eea87e23311d14c6567ff01
ali-sabry <th3man1998@gmail.com>
2022-08-04 21:53:23 +0200

    Remove unnecessary Code

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,16 @@
-// import { Link } from 'react-router-dom';
-// import Navbar from './Style';
 import { Navbar, Logo, Links } from './Style';
 
 const Header = () => (
   <Navbar>
-
     <Logo>
       <img src="/logo.png" alt="logo" />
       <a href="#home">Reddit Timer</a>
     </Logo>
-
     <Links>
       <a href="#search">Search</a>
       <a href="#how-it-work">How It Work</a>
       <a href="#about">About</a>
     </Links>
-
   </Navbar>
 );
 
